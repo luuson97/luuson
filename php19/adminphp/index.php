7caafb30d0acab55029a1cae9cadd900f8e6de03
luuson97 <47914494+luuson97@users.noreply.github.com>
2019-05-15 18:33:03 +0700

    php19

---
+++ php19/adminphp/index.php
@@ -0,0 +1,27 @@
+<?php 
+	require_once('header.php');
+ ?>
+
+
+<div class="container-fluid">
+
+<!-- Breadcrumbs-->
+<ol class="breadcrumb">
+  <li class="breadcrumb-item">
+    <a href="header.php">Trang Chủ</a>
+  </li>
+  <li class="breadcrumb-item active">Trang chủ ZentPHP19</li>
+</ol>
+
+<!-- Page Content -->
+<h1>Trang Chủ Zent Group</h1>
+<hr>
+<p>This is a great starting point for new custom pages.</p>
+
+</div>
+<!-- /.container-fluid -->
+
+
+ <?php 
+ 	require_once('footer.php');
+  ?>
